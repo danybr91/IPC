1f4ab7aaca7fcab46f7cfe4742f22b3a19b42444
Daniel Belén Del Río <dbelendelrio@gmail.com>
2015-11-19 20:40:18 +0100

    Signed-off-by: Daniel Belén Del Río <dbelendelrio@gmail.com>

--- p3/src/main/init.java
+++ p3/src/main/init.java
@@ -2,6 +2,8 @@ package main;
 
 import java.awt.EventQueue;
 
+import javax.swing.UIManager;
+
 import gui.EditorTXT;
 
 public class init {
@@ -10,9 +12,28 @@ public class init {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		// Hilo para la interfaz gráfica. (Se crea por defecto)
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					/*
+					 *  Set the Look And Feel
+					 *  
+					 *  Cambia el Look And Feel de Swing para que utilice el 
+					 *  del sistema si es posible. Si por algún error no puede
+					 *  cargar el estilo del sistema, carga el estilo por
+					 *  defecto.
+					 */
+					if (UIManager.getSystemLookAndFeelClassName()
+							.equals(UIManager
+									.getCrossPlatformLookAndFeelClassName())) {
+			            UIManager.setLookAndFeel("javax."
+			            		+ "swing.plaf.nimbus.NimbusLookAndFeel");
+			        } else {
+			            UIManager.setLookAndFeel(UIManager
+			            		.getSystemLookAndFeelClassName());
+			        }
+					// Crear la vista principal y mostrarla
 					EditorTXT frame = new EditorTXT();
 					frame.setVisible(true);
 				} catch (Exception e) {
